187de637e21d2bd6a4652f6fa3f9ce822a582ea8
fantasticlife <michael@smethur.st:wq>
2023-05-29 11:31:06 +0100

    use base api uri set in application controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,7 @@ class SearchController < ApplicationController
     @form = Form.new
     
     # We construct the URL to grab the data from.
-    uri = "https://search-mock.herokuapp.com/search-mock/form"
+    uri = "#{BASE_API_URI}form"
     
     # We load the data.
     json = JSON.load( URI.open( uri ) )
