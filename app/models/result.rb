9f66489e1eab19e86ca286200574c74245e33ebf
fantasticlife <michael@smethur.st:wq>
2023-06-01 14:30:35 +0100

    just an xml attribute accessor for now

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,8 @@
 class Result
-  attr_accessor :id
-  attr_accessor :title
-  attr_accessor :description
-  attr_accessor :link
+  attr_accessor :xml
+  
+  #attr_accessor :id
+  #attr_accessor :title
+  #attr_accessor :description
+  #attr_accessor :link
 end
