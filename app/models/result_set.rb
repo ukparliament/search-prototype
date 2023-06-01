11165ce30633e61332a427981fe24d5445954ea4
fantasticlife <michael@smethur.st:wq>
2023-06-01 14:32:08 +0100

    added more attribute accessors

--- app/models/result_set.rb
+++ app/models/result_set.rb
@@ -1,6 +1,10 @@
 class ResultSet
+  attr_accessor :query
   attr_accessor :result_count
+  attr_accessor :query_time
   attr_accessor :limit
   attr_accessor :offset
+  attr_accessor :status
+  attr_accessor :version
   attr_accessor :results
 end
