c6d7a557f934a6156e72354abecd505a71ee4d4e
fantasticlife <michael@smethur.st:wq>
2023-06-01 10:56:24 +0100

    search form also at /search-prototype

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
+  get 'search-prototype/' => 'search#form', :as => 'home'
   get 'search-prototype/search' => 'search#form', :as => 'form'
   get 'search-prototype/results' => 'search#results', :as => 'results'
   get 'search-prototype/objects/:object' => 'search#object', :as => 'object_show'
