0ddb537dda322ee7a6c605ce38100f2e593f286e
fantasticlife <michael@smethur.st:wq>
2023-06-02 14:19:05 +0100

    base api url now points to api.parl not heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
   # We set the base API URI.
-  BASE_API_URI = "https://search-mock.herokuapp.com/search-mock/"
+  BASE_API_URI = "https://api.parliament.uk/search-mock/"
   #BASE_API_URI = "http://localhost:3000/search-mock/"
 end
