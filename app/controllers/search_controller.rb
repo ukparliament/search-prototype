87c09fd0c47ae28da1e3878339d141585d403507
fantasticlife <michael@smethur.st:wq>
2023-06-02 14:18:30 +0100

    rewrite to add ability to cope with different document types. refactored parsing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -23,40 +23,20 @@ class SearchController < ApplicationController
   def results
     @page_title = 'Search results'
     
-    # We construct the URL to grab the data from.
-    uri = "#{BASE_API_URI}results"
+    # We get the document type from the URL parameter.
+    document_type = params[:document_type]
+    
+    # We construct the URI to grab the XML from.
+    uri = "#{BASE_API_URI}results/#{document_type}"
     
     # We load the data.
     xml = Nokogiri::XML( URI.open( uri ) )
     
-    # We create a new resultset object.
+    # We create a new result set object.
     @result_set = ResultSet.new
     
-    # We grab the resultset attributes from the data.
-    @result_set.query = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="q"]/text()' ).to_s
-    @result_set.result_count = xml.xpath( 'response/result/@numFound' ).to_s
-    @result_set.query_time = xml.xpath( 'response/lst/int[@name="QTime"]/text()' ).to_s
-    @result_set.offset = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="start"]/text()' ).to_s
-    @result_set.limit = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="rows"]/text()' ).to_s
-    @result_set.status = xml.xpath( 'response/lst/int[@name="status"]/text()' ).to_s
-    @result_set.version = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="version"]/text()' ).to_s
-    
-    # We create an array to hold the results.
-    @result_set.results = []
-    
-    # For each result item returned ...
-    xml.xpath( 'response/result/doc' ).each do |result_document|
-      
-      # ... we create a new result ...
-      result = Result.new
-      
-      # We assign the returned variables.
-      result.display_title = result_document.xpath( 'str[@name="title_t"]/text()' ).to_s
-      result.xml = result_document
-      
-      # ... and add the result to the resultset array.
-      @result_set.results << result
-    end
+    # We parse the results XML.
+    parse_results( xml )
   end
   
   def object
@@ -79,5 +59,54 @@ class SearchController < ApplicationController
     
     # We set the page title to the title of the object.
     @page_title = @search_object.title
+  end
+  
+  # ## A method to parse the result set XML.
+  def parse_results( xml )
+    
+    # We store the returned variables.
+    query = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="q"]/text()' ).to_s
+    result_count = xml.xpath( 'response/result/@numFound' ).to_s
+    query_time = xml.xpath( 'response/lst/int[@name="QTime"]/text()' ).to_s
+    offset = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="start"]/text()' ).to_s
+    limit = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="rows"]/text()' ).to_s
+    status = xml.xpath( 'response/lst/int[@name="status"]/text()' ).to_s
+    version = xml.xpath( 'response/lst/lst[@name="params"]/str[@name="version"]/text()' ).to_s
+    
+    # We assign properties to the result set object.
+    @result_set.query = query
+    @result_set.result_count = result_count
+    @result_set.query_time = query_time
+    @result_set.offset = offset
+    @result_set.limit = limit
+    @result_set.status = status
+    @result_set.version = version
+    
+    # We create an array to hold the results.
+    @result_set.results = []
+    
+    # For each result item returned ...
+    xml.xpath( 'response/result/doc' ).each do |result_document|
+      
+      # ... we parse a result item.
+      parse_result_item( result_document )
+    end
+  end
+  
+  # ## A method to parse the result item XML.
+  def parse_result_item( result_document )
+    
+    # We store the returned variables.
+    title = result_document.xpath( 'str[@name="title_t"]/text()' ).to_s
+    
+    # ... we create a new result object ...
+    result = Result.new
+    
+    # We assign properties to the result object.
+    result.title = title
+    result.xml = result_document
+    
+    # ... and add the result to the resultset array.
+    @result_set.results << result
   end
 end
