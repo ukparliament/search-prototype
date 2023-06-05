56395af165a78fb66559e2a1291866de927c78af
fantasticlife <michael@smethur.st:wq>
2023-06-05 11:43:16 +0100

    xml attribute now called data cos stores xml or json

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,7 +2,7 @@ class Result
   attr_accessor :title
   attr_accessor :uris
   attr_accessor :bibliographic_citations
-  attr_accessor :xml
+  attr_accessor :data
   
   def display_title
     self.title || 'Untitled'
