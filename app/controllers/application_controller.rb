6d1d069dbe977e970bd637c9652094efd04530c2
fantasticlife <michael@smethur.st:wq>
2023-06-06 17:41:18 +0100

    removed api format constant. just ruby now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,4 @@ class ApplicationController < ActionController::Base
   # We set the base API URI.
   BASE_API_URI = "https://api.parliament.uk/search-mock/"
   #BASE_API_URI = "http://localhost:3000/search-mock/"
-  
-  # We set the format we want to parse.
-  API_FORMAT = 'json'
 end
