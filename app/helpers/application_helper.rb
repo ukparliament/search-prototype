0762fe0d45cc2d0e7a06c7c7f830b5896163b3c8
fantasticlife <michael@smethur.st:wq>
2023-06-11 17:36:14 +0100

    new method to construct document type display < dodgy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
   
-  # We set the date display format.
+  # ## We set the date display format.
   DATE_DISPLAY_FORMAT = '%A, %e %B %Y'
+  
+  # ## A method to display the document type based on the SES ID.
+  # NOTE: this should probably be replaced by a SES lookup.
+  def document_type_display( document_type )
+    
+    # We check the document type and display document type label.
+    case document_type
+    when 93522
+      'Written question'
+    when 352211
+      'Written statement'
+    else
+      'Document type unknown'
+    end
+  end
 end
