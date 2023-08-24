5e282266f1e80e67737d4d1a197a7288831ace84
j-corry <jonathan.corry@methods.co.uk>
2023-08-24 14:35:56 +0100

    - Tidy up amendments text so that sentences dependent on missing data items are not shown.

--- spec/requests/edm_spec.rb
+++ spec/requests/edm_spec.rb
@@ -16,15 +16,17 @@ RSpec.describe 'ContentObjects', type: :request do
     test_data = JSON.parse(File.read("spec/fixtures/edm_test_data.json"))
     docs = test_data["response"]["docs"]
 
-    let!(:edm_instance) { Edm.new(first_doc) }
-    let!(:first_doc) { docs.first }
+    docs.each_with_index do |doc, index|
+      let!(:edm_instance) { Edm.new(data) }
+      let!(:data) { doc }
 
-    it 'returns http success' do
-      allow_any_instance_of(ApiCall).to receive(:object_data).and_return('test')
-      allow(ContentObject).to receive(:generate).and_return(edm_instance)
-      get '/search-prototype/objects', params: { :object => edm_instance }
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include("QUESTIONS AND ANSWERS ON THE FLOOR")
+      it 'returns http success' do
+        allow_any_instance_of(ApiCall).to receive(:object_data).and_return('test')
+        allow(ContentObject).to receive(:generate).and_return(edm_instance)
+        get '/search-prototype/objects', params: { :object => edm_instance }
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include("QUESTIONS AND ANSWERS ON THE FLOOR")
+      end
     end
   end
 end
