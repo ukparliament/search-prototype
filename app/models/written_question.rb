5c2c624e40893c30a364fd8cd00e358184aeba61
j-corry <jonathan.corry@methods.co.uk>
2023-09-04 14:56:42 +0100

    - Refactor written question states - Various methods needed for written questions - Prelim partials for written questions - (Work in progress) building out secondary information for written questions

--- app/models/written_question.rb
+++ app/models/written_question.rb
@@ -12,24 +12,87 @@ class WrittenQuestion < ContentObject
     "written question"
   end
 
-  def prelim_partial
+  def state
     return if content_object_data['pqStatus_t'].blank?
 
     state = content_object_data['pqStatus_t']
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_answered.html.erb' if state == 'answered'
+  def tabled?
+    state == 'tabled'
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_tabled.html.erb' if state == 'tabled'
+  def answered?
+    state == 'answered'
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_holding.html.erb' if state == 'holding'
+  def holding?
+    state == 'holding'
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_answered_was_holding.html.erb' if state == 'answered_was_holding'
+  def answered_was_holding?
+    state == 'answered_was_holding'
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_withdrawn.html.erb' if state == 'withdrawn'
+  def withdrawn?
+    state == 'withdrawn'
+  end
 
-    return 'app/views/search/fragments/_written_question_prelim_corrected.html.erb' if state == 'corrected'
+  def corrected?
+    state == 'corrected'
+  end
+
+  def prelim_partial
+    return 'app/views/search/fragments/_written_question_prelim_tabled.html.erb' if tabled?
+
+    return 'app/views/search/fragments/_written_question_prelim_answered.html.erb' if answered?
+
+    return 'app/views/search/fragments/_written_question_prelim_holding.html.erb' if holding?
+
+    return 'app/views/search/fragments/_written_question_prelim_answered_was_holding.html.erb' if answered_was_holding?
+
+    return 'app/views/search/fragments/_written_question_prelim_withdrawn.html.erb' if withdrawn?
+
+    return 'app/views/search/fragments/_written_question_prelim_corrected.html.erb' if corrected?
 
     nil
   end
 
+  def date_of_answer
+    return if content_object_data['dateOfAnswer_dt'].blank?
+
+    content_object_data['dateOfAnswer_dt'].first
+  end
+
+  def tabling_member
+    return if content_object_data['tablingMember_ses'].blank?
+
+    content_object_data['tablingMember_ses'].first
+  end
+
+  def answering_member
+    return if content_object_data['answeringMember_ses'].blank?
+
+    content_object_data['answeringMember_ses'].first
+  end
+
+  def answer_text
+    return if content_object_data['answerText_t'].blank?
+
+    CGI::unescapeHTML(content_object_data['answerText_t'].first)
+  end
+
+  def question_text
+    return if content_object_data['questionText_t'].blank?
+
+    CGI::unescapeHTML(content_object_data['questionText_t'].first)
+  end
+
+  def transferred?
+    return if content_object_data['transferredQuestion_b'].blank?
+
+    return true if content_object_data['transferredQuestion_b'] == 'true'
+
+    false
+  end
 end
