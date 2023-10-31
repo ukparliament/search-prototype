897de267292ccfc79e688c7e51b9d12eab72e406
j-corry <jonathan.corry@methods.co.uk>
2023-10-31 14:38:15 +0000

    - Workaround for known issue with relative_url_root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
+  def url_options
+    super.except(:script_name).merge({ only_path: true })
+  end
+
   # TODO: Move these to appropriate class
   # We set the base API URI.
   BASE_API_URI = "https://api.parliament.uk/search-mock/"
