b8fd83938eb1869fd6947e6edb42b1c0e4a3a4d9
j-corry <jonathan.corry@methods.co.uk>
2024-01-16 15:02:58 +0000

    - Updates to bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -8,6 +8,25 @@ class Bill < ContentObject
     'search/objects/bill'
   end
 
+  def object_name
+    subtype_or_type
+  end
+
+  def date_of_order
+    get_first_as_date_from('dateOfOrderToPrint_dt')
+  end
+
+  def previous_version_link
+    get_first_from('isVersionOf_t')
+  end
+
+  def previous_version
+    previous = get_all_from('isVersionOf_t')&.pluck(:value)
+    return if previous.blank?
+
+    ObjectsFromUriList.new(previous).get_objects[:items]&.first
+  end
+
   def title
     get_as_string_from('title_t')
   end
