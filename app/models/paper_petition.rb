4f79da89bed1315c8501007006dfc30b495e9f18
j-corry <jonathan.corry@methods.co.uk>
2024-01-18 12:50:40 +0000

    - Updates to paper petitions

--- app/models/paper_petition.rb
+++ app/models/paper_petition.rb
@@ -7,4 +7,20 @@ class PaperPetition < ContentObject
   def template
     'search/objects/paper_petition'
   end
+
+  def object_name
+    subtype_or_type
+  end
+
+  def multi_member?
+    members.size > 1
+  end
+
+  def members
+    get_all_from('member_ses')
+  end
+
+  def content
+    abstract_text.blank? ? get_first_as_html_from('petitionText_t') : abstract_text
+  end
 end
