55fd41deefefdd1d95f37a65c6f6d96d9e2f02a4
j-corry <jonathan.corry@methods.co.uk>
2024-06-26 16:59:13 +0100

    - Fix failing tests (further test updates still required)

--- spec/requests/research_briefing_spec.rb
+++ spec/requests/research_briefing_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Research Briefing', type: :request do
     it 'returns http success' do
       allow_any_instance_of(SolrQuery).to receive(:all_data).and_return({ 'response' => { "docs" => ['test'] } })
       allow_any_instance_of(SolrMultiQuery).to receive(:object_data).and_return([])
+      allow_any_instance_of(SesLookup).to receive(:data).and_return({})
       allow(ContentObject).to receive(:generate).and_return(research_briefing_instance)
       allow_any_instance_of(ResearchBriefing).to receive(:ses_data).and_return(research_briefing_instance.type => 'research briefing')
       get '/objects', params: { :object => 'test_string' }
