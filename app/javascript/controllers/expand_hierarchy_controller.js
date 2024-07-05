844fdc3a3e85b385c87f7c4bd126b4a76e4c8e16
j-corry <jonathan.corry@methods.co.uk>
2024-07-05 15:20:32 +0100

    - Add expanded_ids as a parameter for search - Add read expanded_ids into javascript for toggling hierarchy list, so that we can keep track of state by modifying urls elsewhere on the page - Add search data expanded_ids method to read params and determine state of list on page load - Remove pagination view comments

--- app/javascript/controllers/expand_hierarchy_controller.js
+++ app/javascript/controllers/expand_hierarchy_controller.js
@@ -8,9 +8,42 @@ export default class extends Controller {
 
     toggle(event) {
         const container_id = event.params.id
+        const container_id_string = container_id.toString();
+
+        const links = document.querySelectorAll('.modifiable-link');
+        let existingIds = [];
+        const first_link = links.item(0).href;
+        const first_url = new URL(first_link);
+        const first_link_params = first_url.searchParams;
+
+        if (typeof first_link_params !== "undefined") {
+            const ids_from_params = first_link_params.getAll('expanded_ids');
+
+            ids_from_params.forEach(param => {
+                const ids_array = param.split(',')
+                ids_array.forEach(id => {
+                        existingIds.push(id.toString());
+                    }
+                )
+            })
+        }
+
+        if (existingIds.includes(container_id_string)) {
+            const delete_index = existingIds.indexOf(container_id_string);
+            existingIds.splice(delete_index, 1);
+        } else {
+            existingIds.push(container_id_string);
+        }
+
+        links.forEach(link => {
+            const url = new URL(link.href);
+            url.searchParams.delete('expanded_ids');
+            url.searchParams.append('expanded_ids', existingIds);
+            link.href = url.toString();
+        });
+
         const all_toggles = Array.from(document.getElementsByClassName('toggle-' + container_id));
         const button = event.currentTarget;
-
         all_toggles.forEach(element => {
             element.classList.toggle("collapse");
             if (element.classList.contains("collapse")) {
