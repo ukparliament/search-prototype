41786103efa4be7ac615d4cd95865cc135053068
Robert Brook <robertbrook@fastmail.fm>
2024-07-24 14:24:57 +0100

    merge

--- app/models/act.rb
+++ app/models/act.rb
@@ -3,10 +3,15 @@ class Act < ContentObject
   def initialize(content_object_data)
     super
   end
+  
+  # test comment for template
 
   def template
     'search/objects/act'
   end
+  
+  # 	Array.new()
+  # another test comment for name
 
   def object_name
     "Act"
