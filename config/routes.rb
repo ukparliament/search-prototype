538beb6b0194db35a08d42c603df7cd2c9dfa90f
j-corry <jonathan.corry@methods.co.uk>
2025-01-30 15:59:20 +0000

    - Add welcome controller to be new landing page - Add new landing page content - Reformat example links and extract to shared partial; include on new landing page for the time being - Adjust azure production API credentials

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'content_type_objects#index'
-
+  root 'welcome#index'
+  get 'welcome' => 'welcome#index'
+  get 'examples' => 'content_type_objects#index'
   get 'search' => 'search#index'
   get 'objects' => 'content_type_objects#show', as: 'object_show'
   get 'errors/500' => 'errors#internal_server_error'
