0122b082f8fe5ebebe6677bf3fa88dad0488e9c3
j-corry <corryj@parliament.uk>
2026-03-25 17:05:50 +0000

    - Remove unused helper methods - Remove old date facet partial - Update remove_filter_url helper method to clear date_month & date_year values entirely when removing either - Tidy up filter helper generally (WIP) - Modify applied filters to add currently applied date year value to any currently applied date month filter value - Add month_filter_section partial as a modified version of filter_section for use with month facet only

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,6 +1,14 @@
 module FilterHelper
-  def remove_filter_url(params, filter_name, filter_value)
-    params.except(:page).merge(filter: params[:filter].merge({ filter_name => params.dig(:filter, filter_name)&.excluding(filter_value) }))
+  def remove_filter_url(params_hash, filter_name, filter_value)
+    baseline_params = params_hash.except(:page)
+
+    if %w[date_month date_year].include?(filter_name)
+      # because date filtering is emulating a drilldown, we want the remove filter link to clear all month/year filters
+      baseline_params.merge(filter: params_hash.dig(:filter).merge({ 'date_month' => [], 'date_year' => [] }))
+    else
+      other_values_for_current_filter = params_hash.dig(:filter, filter_name)&.excluding(filter_value)
+      baseline_params.merge(filter: params_hash.dig(:filter).merge({ filter_name => other_values_for_current_filter }))
+    end
   end
 
   def apply_filter_url(params, filter_name, filter_value)
@@ -9,13 +17,23 @@ module FilterHelper
     params.except(:page).merge(filter: params.dig(:filter).nil? ? { filter_name => [filter_value] } : params.dig(:filter).merge(filter_name => [filter_value, params.dig(:filter, filter_name)].compact.flatten))
   end
 
-  def replace_filter_url(params, filter_name, filter_value)
-    # used for year-month filters; performs the additional step of removing year filters first
-    adjusted_params = params.except(:filter, :year)
-    apply_filter_url(adjusted_params, filter_name, filter_value)
+  def visible_filters
+    params[:filter].has_key?('date_month') ? params[:filter].except('date_year') : params[:filter]
   end
 
-  def year_filter
-    params.dig(:filter, :year)
+  def applied_filters
+    params[:filter]
+  end
+
+  def current_month_filter_value
+    return unless applied_filters
+
+    applied_filters.dig('date_month')&.first
+  end
+
+  def current_year_filter_value
+    return unless applied_filters
+
+    applied_filters.dig('date_year')&.first
   end
 end
