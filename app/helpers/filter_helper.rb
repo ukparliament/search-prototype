193b3fb4d0644c8cbbb606fd9fa7871f9a7483c6
j-corry <corryj@parliament.uk>
2026-03-26 12:57:51 +0000

    - Add filter_helper_spec.rb

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,20 +1,23 @@
 module FilterHelper
   def remove_filter_url(params_hash, filter_name, filter_value)
     baseline_params = params_hash.except(:page)
-
     if %w[date_month date_year].include?(filter_name)
       # because date filtering is emulating a drilldown, we want the remove filter link to clear all month/year filters
-      baseline_params.merge(filter: params_hash.dig(:filter).merge({ 'date_month' => [], 'date_year' => [] }))
+      url_params = baseline_params.merge(filter: params_hash.dig(:filter).merge({ 'date_month' => [], 'date_year' => [] }))
     else
       other_values_for_current_filter = params_hash.dig(:filter, filter_name)&.excluding(filter_value)
-      baseline_params.merge(filter: params_hash.dig(:filter).merge({ filter_name => other_values_for_current_filter }))
+      url_params = baseline_params.merge(filter: params_hash.dig(:filter).merge({ filter_name => other_values_for_current_filter }))
     end
+
+    url_for(url_params)
   end
 
   def apply_filter_url(params, filter_name, filter_value)
     # example output:
     # {"query"=>"horses", "commit"=>"Search", "controller"=>"search", "action"=>"index", "filter"=>{"type_sesrollup"=>[90996]}}
-    params.except(:page).merge(filter: params.dig(:filter).nil? ? { filter_name => [filter_value] } : params.dig(:filter).merge(filter_name => [filter_value, params.dig(:filter, filter_name)].compact.flatten))
+    url_params = params.except(:page).merge(filter: params.dig(:filter).nil? ? { filter_name => [filter_value] } : params.dig(:filter).merge(filter_name => [filter_value, params.dig(:filter, filter_name)].compact.flatten))
+
+    url_for(url_params)
   end
 
   def visible_filters
@@ -29,11 +32,5 @@ module FilterHelper
     return unless applied_filters
 
     applied_filters.dig('date_month')&.first
-  end
-
-  def current_year_filter_value
-    return unless applied_filters
-
-    applied_filters.dig('date_year')&.first
   end
 end
