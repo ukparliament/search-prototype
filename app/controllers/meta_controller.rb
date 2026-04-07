ef16ed69d789ae97772354732be2556d6eb5b7b1
fantasticlife <michael@smethur.st>
2026-04-07 10:30:24 +0100

    constructed meta crumbs and added @heading_one for h1 display

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -3,28 +3,50 @@ class MetaController < ApplicationController
   def index
     @page_title = 'About this website'
     @description = 'About this website.'
-    @crumb << { label: 'About this website', url: nil }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
   end
 
   def cookies
     @page_title = 'Cookies'
     @description = 'Cookie Policy.'
-    @crumb << { label: 'Meta', url: meta_list_url }
-    @crumb << { label: 'Cookies', url: nil }
+    @crumb << { label: 'About this website', url: meta_list_url }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
 
     render 'library_design/meta/cookies'
   end
-
+  
+  def coverage
+    @page_title = 'Coverage'
+    @description = 'Coverage.'
+    @crumb << { label: 'About this website', url: meta_list_url }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
+  end
+  
   def examples
-    @page_title = 'Examples'
+    @page_title = 'Example object pages'
+    @description = 'Example object pages.'
+    @crumb << { label: 'About this website', url: meta_list_url }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
+  end
+  
+  def librarian_tools
+    @page_title = 'Librarian tools'
+    @description = 'Librarian tools.'
+    @crumb << { label: 'About this website', url: meta_list_url }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
   end
 
   def roadmap
     @page_title = 'Roadmap'
-  end
-
-  def coverage
-    @page_title = 'Coverage'
+    @description = 'Roadmap.'
+    @crumb << { label: 'About this website', url: meta_list_url }
+    @crumb << { label: @page_title, url: nil }
+    @heading_one = @page_title
   end
 
 end
